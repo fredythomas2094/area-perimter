00f1249c2254713d88858a8689f2f0ff06eb4f79
fredythomas2094 <59721291+fredythomas2094@users.noreply.github.com>
2020-01-10 16:33:28 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #define pi 3.14
+#define A (pi*r*r);
 int main()
 {
 int r;
 float A,p;
 printf("enter the values of radius:");
 scanf("%d",&r);
-A=(pi*r*r);
+
 p=(2*pi*r);
 printf("AREA =%f\n",;A);
 printf("PERIMETER=%f",p);
