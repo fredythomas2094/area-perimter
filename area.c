c84a95aaddf6852f9a998791321fe66821542ce2
fredythomas2094 <59721291+fredythomas2094@users.noreply.github.com>
2020-01-10 16:25:58 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int r;
+float A,p,pi=3.14;
+printf("enter the values of radius:");
+scanf("%d",&r);
+A=(pi*r*r);
+p=(2*pi*r);
+printf("AREA =%f\n",;A);
+printf("PERIMETER=%f",p);
+return 0;
+}
